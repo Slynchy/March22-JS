20e50f4a5eda63c212e624331423455d084e37f9
Sam Lynch <sam.lynch2@hotmail.co.uk>
2018-03-28 11:01:25 +0200

    Add initialization checks to InputHandler

--- src/engine/handlers/InputHandler.js
+++ src/engine/handlers/InputHandler.js
@@ -1,12 +1,20 @@
 class InputHandler {
 	constructor(){
 
-		if(this.gameMode){
+		this._inputInitialized = false;
+
+		if(Settings.gameMode){
 			this.setupGameKeys();
 		}
 	};
 
+	isInitialized(){
+		return this._inputInitialized;
+	}
+
 	setupGameKeys(){
+		this._inputInitialized = true;
+
         window.addEventListener('keydown', (ev)=>{
             switch(ev.which || ev.keyCode){
                 case 13:
@@ -17,6 +25,8 @@ class InputHandler {
 	}
 
 	handleNewLine(){
+		// TODO: use Settings.debugMode to allow devs to skip anything
+
 		if(M22.SceneHandler.textBox.hasFinishedLine())
 			M22.ScriptHandler.NextLine();
 		else
